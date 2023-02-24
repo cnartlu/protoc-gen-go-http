a3c8f3aeed2917e43428efea4f67a2cd5071baf9
huanghu <huanghu@xingyun.net>
2023-02-24 17:58:01 +0800

    <更新>echo框架路由注册逻辑修改

--- echo.go
+++ echo.go
@@ -9,29 +9,32 @@ type {{$svrType}}ServiceRegistrar interface {
 }
 
 func Register{{.ServiceType}}HttpServer(r {{$svrType}}ServiceRegistrar, srv {{$svrType}}Server) {
-	{{$svrType}}_ServiceHttpDesc = srv
 	{{- range .Methods}}
-	r.Add("{{.Method}}", "{{.Path}}", _{{$svrType}}_{{.Name}}{{.Num}}_HTTP_Handler)
+	r.Add("{{.Method}}", "{{.Path}}", _{{$svrType}}_{{.Name}}{{.Num}}_HTTP_Handler(srv))
 	{{- end}}
 }
 
 {{range .Methods}}
-func _{{$svrType}}_{{.Name}}{{.Num}}_HTTP_Handler(c v4.Context) error {
-	var req *{{.Request}} = new({{.Request}})
-	ctx := c.Request().Context()
-	if err := c.Bind(req{{.Body}}); err != nil {
-		return err
+func _{{$svrType}}_{{.Name}}{{.Num}}_HTTP_Handler(srv {{$svrType}}Server) v4.HandlerFunc {
+	return func(c v4.Context) error {
+		var req *{{.Request}} = new({{.Request}})
+		ctx := c.Request().Context()
+		if err := c.Bind(req{{.Body}}); err != nil {
+			return err
+		}
+		if err := c.Validate(req{{.Body}}); err != nil && err != v4.ErrValidatorNotRegistered {
+			return err
+		}
+		res, err := srv.{{.Name}}(context.WithValue(ctx, v4.DefaultBinder{}, c), req)
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, res)
 	}
-	if err := c.Validate(req{{.Body}}); err != nil && err != v4.ErrValidatorNotRegistered {
-		return err
-	}
-	res, err := {{$svrType}}_ServiceHttpDesc.{{.Name}}(context.WithValue(ctx, v4.DefaultBinder{}, c), req)
-	if err != nil {
-		return err
-	}
-	return c.JSON(http.StatusOK, res)
 }
 {{end}}
+`
 
-var {{$svrType}}_ServiceHttpDesc {{$svrType}}Server = Unimplemented{{$svrType}}Server{}
+var echoMessageTemplate = `
+
 `
